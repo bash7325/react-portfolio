bf79d63877f57432f47f0e6a62de2f7535daa036
Brandon Ashby <bash7325@gmail.com>
2021-04-29 11:52:14 -0600

    navbar working, logo responsiveness issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Button from './Button'
+import { Button } from './Button'
+import './Navbar.css'
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -17,14 +18,18 @@ function Navbar() {
       }
   };
 
+  useEffect(() => {
+      showButton()
+  }, []);
+
   window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            <img src="/images/BA-logo2.png" alt="logo" />
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <img src="/images/BA-logo3.png" alt="logo" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
