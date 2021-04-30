c8be4190e913293c65aaecec2239d2c4fc792582
Brandon Ashby <bash7325@gmail.com>
2021-04-29 21:13:08 -0600

    uncomment

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import '../../App.css';
-import Cards from '../Cards'
-import Footer from '../Footer'
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import Footer from "../Footer";
 
 export default function Projects() {
-    return (
+  return (
     <>
-    <h1 className="projects">PROJECTS</h1>
-    <Cards />
-<Footer />
+      <h1 className="projects">PROJECTS</h1>
+      <Cards />
+      <Footer />
     </>
-    )
+  );
 }
