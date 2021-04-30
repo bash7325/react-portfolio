6265d7948ee6ecb2ef1fe47aad13de80e2b59136
Brandon Ashby <bash7325@gmail.com>
2021-04-29 19:54:14 -0600

    routes working, need to move cards into projects.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
      <Navbar />
      <Switch>
        <Route path='/' exact component={Home}  />
+       <Route path='/projects' component={Projects} />
+       <Route path='/about' component={About} />
+       <Route path='/contact' component={Contact} />
      </Switch>
      </Router>
     </>
