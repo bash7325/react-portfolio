b8b9507d21d3f6346cd9c33fc86a4f045c84aedb
Brandon Ashby <bash7325@gmail.com>
2021-04-29 20:13:34 -0600

    created contact card for email

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../../App.css';
+import ContactCard from '../ContactCard'
 
 export default function Contact() {
-    return <h1 className="contact">CONTACT</h1>
+    return (
+    <>
+    <h1 className="contact">CONTACT</h1>
+    <ContactCard />
+    </>
+    )
 }
