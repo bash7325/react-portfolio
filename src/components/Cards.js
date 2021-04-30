608751a510fe9ad20246154ac5aca3af1693739d
Brandon Ashby <bash7325@gmail.com>
2021-04-29 19:24:23 -0600

    cards ready for props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
+import CardItem from "./CardItem";
+import './Cards.css';
 
 function Cards() {
-    return (
-        <div className="cards">
-            <h1>Check out my projects</h1>
-            <div className="cards__container">
-            <div className="cards__wrapper">
-                <ul className="cards__items">
-
-                </ul>
-            </div>
-            </div>
+  return (
+    <div className="cards">
+      <h1>Check out my projects</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+              <CardItem />
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
