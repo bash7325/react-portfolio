429f777ad060815f5cf12943a15db090a81bc5ed
Brandon Ashby <bash7325@gmail.com>
2021-04-29 20:49:50 -0600

    added footer component to all pages

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import ContactCard from '../ContactCard'
+import Footer from '../Footer'
 
 export default function Contact() {
     return (
     <>
     <h1 className="contact">CONTACT</h1>
     <ContactCard />
+    <Footer />
     </>
     )
 }
