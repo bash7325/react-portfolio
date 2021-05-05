1fccfcd3049100d17ce0cde108fc6d8c054bea35
Brandon Ashby <bash7325@gmail.com>
2021-05-04 18:33:31 -0600

    fixed css for new about page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,20 +5,37 @@ import "./AboutMe.css";
 
 function AboutMe() {
   return (
-    <div className="hero-container">
-    <video src="/videos/bkgrd-video.mp4" autoPlay loop muted />
-    <h1>Hello</h1>
-    <p>I'm Brandon</p><p> Welcome to my portfolio</p>
-    <div className="hero-btns">
-      <Button
-        className="btns"
-        buttonStyle="btn--outline"
-        buttonSize="btn--large"
-      >
-        CONTACT ME
-      </Button>
+    <div className="about-container">
+      <h1>Hello</h1>
+      <p>
+        A current student in the University of Utah’s full stack web development
+        bootcamp currently honing skills in HTML, CSS, Javascript, popular
+        frameworks and third-party API’s. I have a rich work history varying
+        from leading teams as an Executive Chef and manager in high volume, fine
+        dining restaurants to complex problem solving and situational awareness
+        as an EMT. I am passionate about creating experiences that are elevated
+        for the user and the process that goes into reaching that goal. I am
+        eager to combine my background in leadership and problem solving with my
+        newly acquired technical skills to help grow capable, dynamic teams and
+        build great applications.
+      </p>
+      <p>
+        I love to spend time with my dog, Riley and my family. I enjoy exploring
+        the mountains and deserts of Utah hiking, backpacking, paddle boarding
+        and fishing. I am intrigued by anything tech related. I love hands on
+        activities i.e. gaming, tinkering with computers, woodwork and working
+        on cars to name a few.
+      </p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          CONTACT ME
+        </Button>
+      </div>
     </div>
-  </div>
   );
 }
 
