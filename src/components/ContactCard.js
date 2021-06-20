de6fc5c4f0875213d5566b4d984cecac0b703888
Brandon Ashby <bash7325@gmail.com>
2021-06-20 09:49:23 -0600

    emailJS working, needs styling

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import './ContactCard.css';
 import { Button } from './Button';
+import emailjs from 'emailjs-com';
 
-function ContactCard() {
+
+export default function ContactCard() {
+
+function sendEmail(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_vitfz9l', 'template_jmiqd7g', e.target, 'user_D7D5vrrsX9nMGOnxFWy35')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
   return (
-    <div className='contact-container'>
-      <section className='contact-subscription'>
-        <p className='contact-subscription-heading'>
-          <p>Questions?  Please contact me using the form below</p>
-          Email me at: bash7325@gmail.com or call/text at: 435-503-6255
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='email-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Send</Button>
-          </form>
-        </div>
-      </section>
-    </div>
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
-}
+  }
 
-export default ContactCard;
