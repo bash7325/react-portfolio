3aa48518c1476aa4a2bf5d89f1716b0a97efa93e
bash7325 <bash7325@gmail.com>
2021-09-15 11:30:12 -0600

    remove unused components, update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,14 +11,13 @@ function AboutMe() {
         <p>
           A recent graduate of the University of Utah's Full Stack Web
           Development program. I am currently working as a developer focused on
-          maintaining and building front-end applications in HTML/CSS, SQL, C#
-          and Javascript. I am also studying AWS, React and Python. I've been
+          maintaining and building web applications in HTML/CSS, SQL, C#
+          and Javascript. I've been
           able to apply my new knowledge to personal projects and hobbies.
         </p>
         <p>
           I enjoy exploring the mountains and deserts of Utah hiking,
-          backpacking, paddle boarding and fishing. I am intrigued by anything
-          tech related. I love hands on activities i.e. gaming, tinkering with
+          backpacking, paddle boarding and fishing. I love hands on activities i.e. gaming, building
           computers, woodwork and working on cars to name a few.
         </p>
         <div className="hero-btns">
